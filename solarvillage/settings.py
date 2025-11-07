62e6eaa337762b546616d94e6f1b39e6982b06f6
Coffee Boy <connorkchris@gmail.com>
2025-11-07 11:20:57 -0800

    Use environment variable for database name

--- solarvillage/settings.py
+++ solarvillage/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'solarvillage.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': os.getenv('DB_NAME', 'postgres'),
         'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
